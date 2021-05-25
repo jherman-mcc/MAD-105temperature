1f40e0ab48427b0c66b98f483a84c942bfb581f7
JH <https://github.com/jherman-mcc/MAD-105aboutme.git>
2021-05-24 22:41:21 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,18 @@
+fun main (args: Array<String>) {
+    var temp = 80
+    var low = 97.5
+    var high = 99.5
+
+    While (temp != 0) {
+        print("Enter the current temperature: ")
+        temp = readLine()!! toInt ()
+        if temp < 97.5
+            println("Temperature is $temp and it is low")
+        else if (temp > 99.5)
+            println("Temperature is $temp and it is high")
+        else
+            println("Temperature is $temp and it is normal")
+    }
+
+
+}
