85adcea1613cf1bd8e9b55597420f5f7492350d7
JH <https://github.com/jherman-mcc/MAD-105aboutme.git>
2021-05-24 22:41:21 -0500

    Second commit

--- src/main.kt
+++ src/main.kt
@@ -1,12 +1,14 @@
+import kotlin.math.abs
+
 fun main (args: Array<String>) {
     var temp = 80
     var low = 97.5
     var high = 99.5
 
-    While (temp != 0) {
+    while (temp != 0) {
         print("Enter the current temperature: ")
-        temp = readLine()!! toInt ()
-        if temp < 97.5
+        temp = readLine()!!.toInt()
+        if (temp < 97.5)
             println("Temperature is $temp and it is low")
         else if (temp > 99.5)
             println("Temperature is $temp and it is high")
